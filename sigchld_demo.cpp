7c41163f0801bfa9d0e4982a80795ed1199bb8b9
fanqie <gyx645378029@163.com>
2019-09-13 14:23:19 +0800

    SIGCHLD

---
+++ sigchld_demo.cpp
@@ -0,0 +1,59 @@
+/**
+ * SIGCHLD 子进程结束时向父进程发送的信号
+ * 父进程需要接受此信号，否则子进程会成为僵尸进程
+ */
+#include <unistd.h>
+#include <stdio.h>
+#include <signal.h>
+#include <sys/wait.h>
+
+void sighandler(int sig_no) {
+    int status;
+    pid_t pid;
+    /**
+     * 坑点  if ((pid = waitpid(-1, &status, 0)) { .. }
+     * 子进程结束时，handler正在执行，会将多个SIGCHLD合并成一个
+     */
+    while ((pid = waitpid(-1, &status, 0)) > 0) {
+        //进程被恢复执行
+        if (WIFCONTINUED(status)) {
+            printf("child %d continue\n", pid);
+        }
+        //进程正常退出
+        else if (WIFEXITED(status)) {
+            printf("child %d exit\n", pid);
+        }
+        //进程被信号终止
+        else if (WIFSIGNALED(status)) {
+            printf("child %d signaled\n", pid);
+        }
+        //进程被暂停执行
+        else if (WIFSTOPPED(status)) {
+            printf("child %d stopped\n", pid);
+        }
+    }
+}
+
+int main() {
+    if (signal(SIGCHLD, sighandler) == SIG_ERR) {
+        perror("signal SIGSTOP\n");
+    }
+    int fork_pid;
+    for (int i = 0; i < 10; ++i) {
+        if ((fork_pid = fork()) == 0) {
+            break;
+        } else {
+            printf("创建子进程 pid %d\n", fork_pid);
+        }
+    }
+    if (fork_pid == 0) {
+        printf("I'm child no.%d\n", getpid());
+        sleep(10);
+        return 0;
+    }
+    while (true) {
+        write(STDOUT_FILENO, ".", 1);
+        sleep(5);
+    }
+    return 0;
+}
