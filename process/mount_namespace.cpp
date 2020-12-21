94440d9ea1d662f492630874b2c7d54306aa848e
tomato <root@localhost.localdomain>
2020-12-21 10:04:32 -0500

    mount

--- process/mount_namespace.cpp
+++ process/mount_namespace.cpp
@@ -4,10 +4,13 @@
 #include <sched.h>
 #include <signal.h>
 #include <wait.h>
-#define STACK_SIZE (1024 * 1024)
+#include <sys/mount.h>
+const int STACK_SIZE = 1024 * 1024;
 
 static int child_func(void* arg) {
     char* const cmd[] = {"/bin/bash", NULL};
+    mount("", "/", NULL, MS_PRIVATE, "");
+    mount("none", "/root/cdemo/UNIX_DEMO/rootfs", "tmpfs", 0, "");
     execvp(cmd[0], cmd);
     return 0;
 }
@@ -15,8 +18,8 @@ static int child_func(void* arg) {
 int main(int argc, char const *argv[]) {
     void* const buffer = malloc(STACK_SIZE);
     void* const stack_top = buffer + STACK_SIZE;
-    if (clone(child_func, stack_top, CLONE_NEWNS | SIGCHLD, NULL) != 0) {
-        int child_pid;
+    int child_pid;
+    if ((child_pid = clone(child_func, stack_top, CLONE_NEWNS | SIGCHLD, NULL)) != 0) {
         while ((child_pid = wait(NULL)) != -1) {
             printf("child process %d exited\n", child_pid);
         }
